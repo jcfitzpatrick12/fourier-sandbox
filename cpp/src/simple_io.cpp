77e46dcb223056184ef875b7e1008ae6ecb5113c
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-11-11 21:28:08 +0000

    feat: implement write to file

--- cpp/src/simple_io.cpp
+++ cpp/src/simple_io.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
+#include <fstream>
 #include "types.h"
 
-void write_to_file(const complex_vector& f)
+void write_to_file(const complex_vector& v)
 {
-    std::cout << "Here!" << "\n";
-    const complex_double* ptr { f.data() };
-    for (int i {0}; i < f.size(); ++i)
-    {
-        std::cout << *(ptr + i) << "\n";
-    }
+    std::ofstream file;
+    file.open("./file.bin", std::ios_base::binary);
+    const char* char_ptr { reinterpret_cast<const char*>( v.data() ) };
+    file.write(char_ptr, sizeof(complex_double) * v.size());
 }
